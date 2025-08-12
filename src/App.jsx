cdb0f064f9484e26bab83a4a809079611b7d05fa
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-08-12 15:15:26 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./assets/components/navbar";
+import Heropage from "./assets/components/heropage";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Heropage />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/components/heropage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Heropage = () => {
+  return (
+    <div>
+      <h1>this section is for hero page</h1>
+    </div>
+  );
+};
+
+export default Heropage;
